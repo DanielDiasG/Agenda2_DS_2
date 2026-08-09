8a8da116e596f8a20ab4ed2a4630455d109274d3
Daniel Dias Gomes <daniel.dias.g08@gmail.com>
2026-08-08 23:56:39 -0300

    Add files via upload

--- processaCadastro.php
+++ processaCadastro.php
@@ -1,11 +1,13 @@
+// style para a estilização do php //
 <style>
+    /*margem e cor de fundo*/
 body{
     margin-left: 30px;
     margin-right: 30px;
     background-color: #e1cfcf;
 }
 
-
+/* estilo para o título */
 h1 {
     font-family: Arial, Helvetica, sans-serif;
     text-align: center;
@@ -17,7 +19,7 @@ h1 {
 }
 
 
-
+ /* estilo para a div de fundo da imagem*/
 div {
     margin-left: 20%;
     margin-right: 20%;
@@ -26,6 +28,8 @@ div {
     border-radius: 10px;
 
 }
+
+/* estilo para o input de voltar*/
 input{
     padding: 5px;
     margin: 5px;
@@ -36,12 +40,13 @@ input{
     text-shadow: 2px 2px 5px #d3c67a;
     border: none;
     padding: 10px 20px;
-        display: block;
+    display: block;
     margin: 0 auto;
     box-sizing: border-box;
 
 }
 
+/* estilo para a imagem */
 img{
     border-radius: 10px;
     box-shadow: 5px 5px 10px #ffffff;
@@ -49,6 +54,8 @@ img{
     
     
 }
+
+/* estilo para o parágrafo */
 p{ text-align: center;
     color: #100f0f;
     background-color: #ffffff; 
@@ -58,7 +65,7 @@ p{ text-align: center;
 
 
 }
-
+/* estilo para a div de informações */
 #informacoes{
     text-align: center;
     background-color: #ffffff; 
@@ -68,17 +75,25 @@ p{ text-align: center;
 }
 </style>
 
+/* php */
 <?php
-
+   /* título */
 echo "<h1>Cadastro realizado com sucesso!</h1>";
+
+/* informações do cadastro - criação das variáveis ------------------------------------------------------- */
 echo "<div id='informacoes'><b>Nome:</b> " .$_POST['nome']. "<br>";
 echo "<b>Idade:</b> " .$_POST['idade']. "<br>";
 echo "<b>Profissão:</b> " .$_POST['prof']. "<br>";
 echo "<b>Salário pretendido:</b> " .$_POST['salpre']. "<br>";
 echo "<b>Experiência anterior:</b> " .$_POST['expant']. " </div><br>";
+
+
+/* agradecimento pelo preenchimento do formulário */
 echo "<p>Obrigado ".$_POST['prof'].", ".$_POST['nome']. " pela colaboração, a loja Brincos e Companhia agradece! <br> 
 experiência: ".$_POST['expant'].".</p>";
 
+/* botão de voltar */
 echo "<input type='button' value='Voltar' onclick='history.go(-1)'> <br><br>";
 
+/* imagem da loja*/
 echo "<div><img src='Loja_chat.png' alt='Fundo da página' width='80%' height='80%'></div>";
